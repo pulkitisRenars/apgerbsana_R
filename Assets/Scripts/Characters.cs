6ed0c038b51497774e7ea99d6d15b64631c35c3c
renars <renars.pulkis@gmail.com>
2022-02-23 15:36:58 +0200

    Kļūdu pielabošana

--- Assets/Scripts/Characters.cs
+++ Assets/Scripts/Characters.cs
@@ -6,11 +6,18 @@ using UnityEngine.UI;
 public class Characters : MonoBehaviour {
 	public Sprite[] charactersArr;
 	public GameObject attelaTuretajs;
+	public GameObject infoF;
+	public GameObject infoT;
 
 	public void izkritosais(int skaitlis){
-		if (skaitlis == 0)
+		if (skaitlis == 0){
+			infoT.SetActive (false);
 			attelaTuretajs.GetComponent<Image> ().sprite = charactersArr [0];
-		else if (skaitlis == 1)
+			infoF.SetActive (true);
+		}else if (skaitlis == 1){
+			infoF.SetActive (false);
 			attelaTuretajs.GetComponent<Image> ().sprite = charactersArr [1];
+			infoT.SetActive (true);
+		}
 	}
 }
