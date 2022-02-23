3cad5e8f59408c4d876c1c0c82d5737a8629a0d7
renars <renars.pulkis@gmail.com>
2022-02-23 12:13:51 +0200

    Ievade

---
+++ Assets/Scripts/Characters.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Characters : MonoBehaviour {
+	public Sprite[] charactersArr;
+	public GameObject attelaTuretajs;
+
+
+	public void izkritosais(int skaitlis){
+		if (skaitlis == 0)
+			attelaTuretajs.GetComponent<Image> ().sprite = charactersArr [0];
+		else if (skaitlis == 1)
+			attelaTuretajs.GetComponent<Image> ().sprite = charactersArr [1];
+		else if (skaitlis == 2)
+			attelaTuretajs.GetComponent<Image> ().sprite = charactersArr [2];
+	}
+}
---
+++ Assets/Scripts/izvaditVardu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class izvaditVardu : MonoBehaviour {
+	public string vards;
+	public string gadi;
+	public GameObject ievadesLauksV;
+	public GameObject ievadesLauksG;
+	public GameObject izvadesLauks;
+
+	public void vardaIzvade(){
+		vards = ievadesLauksV.GetComponent<Text> ().text;
+		gadi = ievadesLauksG.GetComponent<Text> ().text;
+		izvadesLauks.GetComponent<Text>().text = "Varoņa vārds ir "+vards+" un varonim ir "+gadi+" gadu!";
+	}
+
+
+}
